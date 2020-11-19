e71a75d117687d394338d53c0b7c38457d172580
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2020-11-19 22:10:48 +0700

    add: campaign FindAll,FindByUserID

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,6 @@
 package handler
+
+// tangkap parameter di handler
+// handler ke service
+// service menentukan repository mana yang akan dipanggil
+// repository mengakses ke db : GetAll,GetByUserID
